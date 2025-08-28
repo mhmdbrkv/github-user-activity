6876134a676eaf964013bfa2cdf95683ede6b4fc
mohamed baraka <barakamohamed946@gmail.com>
2025-08-28 16:29:30 +0300

    add logic into index.js

--- index.js
+++ index.js
@@ -10,16 +10,50 @@ try {
 
   const data = await fetch(url);
   if (!data.ok) {
-    console.log("Error fetching data: Invalid username");
+    console.log("Error fetching data: Invalid username or invalid API");
     process.exit(1);
   }
 
   const events = await data.json();
-  const commits = events
-    .filter((event) => event.type === "PushEvent")
-    .map((event) => event.payload.commits[0].message);
 
-  console.log(commits.join("\n"));
+  events?.forEach((element) => {
+    switch (element.type) {
+      case "PushEvent":
+        console.log(
+          `- Pushed ${events[0].payload.commits.length} commits to ${events[0].repo.name}`
+        );
+        break;
+
+      case "CreateEvent":
+        console.log(`- Created ${events[0].repo.name}`);
+        break;
+
+      case "DeleteEvent":
+        console.log(`- Deleted ${events[0].repo.name}`);
+        break;
+
+      case "ForkEvent":
+        console.log(`- Forked ${events[0].repo.name}`);
+        break;
+
+      case "WatchEvent":
+        console.log(`- Watched ${events[0].repo.name}`);
+        break;
+
+      case "IssuesEvent":
+        console.log(
+          `- ${events[0].payload.action} an issue in ${events[0].repo.name}`
+        );
+        break;
+
+      case "StarEvent":
+        console.log(`- Starred ${events[0].repo.name}`);
+        break;
+
+      default:
+        break;
+    }
+  });
 } catch (err) {
   console.log("Error fetching data: ", err);
 }
